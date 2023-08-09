d42bb7422d409baa79a3c2fb9e9d96ccdf1b5c0e
6youngEH <a42230@naver.com>
2023-08-09 16:23:19 +0900

    coding test

---
+++ src/Coding_test/ChangeString.java
@@ -0,0 +1,21 @@
+package Coding_test;
+
+import java.util.Scanner;
+
+class ChangeString {
+    public String solution(String str){
+        String answer = "";
+        for(char x : str.toCharArray()){
+            if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
+            else answer+=Character.toLowerCase(x);
+
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        ChangeString T = new ChangeString();
+        Scanner s = new Scanner(System.in);
+        String str = s.next();
+        System.out.println(T.solution(str));
+    }
+}
--- src/Coding_test/FindCharacter.java
+++ src/Coding_test/FindCharacter.java
@@ -2,26 +2,27 @@ package Coding_test;
 
 
 import java.util.Scanner;
-class Main{
 
-    public int solution(String str, char t){
+class FindCharacter {
+    public int solution(String str, char t) {
         int answer = 0;
         str = str.toUpperCase();
         t = Character.toUpperCase(t);
-        //System.out.println(str+" "+t);
-        for(int i = 0; i<str.length();i++){
-            if(str.charAt(i)==t) answer++;
+        for (char x : str.toCharArray()) {
+            if (x == t) {
+                answer++;
+            }
         }
-
         return answer;
     }
 
     public static void main(String[] args) {
-        Main T= new Main();
         Scanner s = new Scanner(System.in);
         String str = s.next();
         char c = s.next().charAt(0);
-        System.out.println(T.solution(str,c));
 
+        FindCharacter findCharacter = new FindCharacter();
+        System.out.println(findCharacter.solution(str, c));
     }
 }
+
